25b4c9834fce2daab71b0bec18d3c56999d2f58c
Carol Zhou <xiaoxue-zhou@uiowa.edu>
2022-04-02 13:53:21 -0500

    udpate

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/mike')
+def mike():
+    return render_template('mike.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
