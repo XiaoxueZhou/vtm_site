a3cddec06cd7d930b73c9a63d70fdbf41779b533
Carol Zhou <xiaoxue-zhou@uiowa.edu>
2022-04-25 14:58:47 -0500

    pytest

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -9,16 +9,14 @@ def test_about_route(app, client):
         assert res.status_code == 200
 
 def test_estimate_route(app, client):
-    prnt("-- /estimate GET test")
     with app.test_client() as test_client:
         res = test_client.get('/estimate')
-        assert res.status_code == 302
-        assert res.headers["Welcome to Estimate Page"] == "/estimate"
+        assert res.status_code == 200
 
 def test_estimate_functionality(app, client):
-    prnt("-- /estimate POST test")
+    print("-- /estimate POST test")
     with app.test_client() as test_client:
         estimate = {"radius":"180", "height":"360"}
         res = test_client.post('/estimate', data=estimate)
-        assert res.status_code == 302
+        assert res.status_code == 200
         assert b"141,300"
